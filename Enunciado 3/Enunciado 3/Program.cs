c30681de4e353a289f2267ef3d7462fb6cdb257f
Joan <jmartinezr30@miumg.edu.gt>
2025-02-14 19:46:16 -0600

    Update Program.cs

--- Enunciado 3/Enunciado 3/Program.cs
+++ Enunciado 3/Enunciado 3/Program.cs
@@ -4,18 +4,18 @@ class Program
 {
     static void Main()
     {
-        // Datos de acceso correctos
+    
         string usuarioCorrecto = "joan";
         string contraseñaCorrecta = "1234";
 
-        // Pedir usuario y contraseña al usuario
+        
         Console.Write("Ingresa tu usuario: ");
         string usuario = Console.ReadLine();
 
         Console.Write("Ingresa tu contraseña: ");
         string contraseña = Console.ReadLine();
 
-        // Verificar credenciales
+        
         if (usuario == usuarioCorrecto && contraseña == contraseñaCorrecta)
         {
             Console.WriteLine("Acceso concedido.");
